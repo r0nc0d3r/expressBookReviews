ab1062336c972292d8bf4e0342356bad0dc635aa
R0N-C0D3R <roncoder261@gmail.com>
2023-09-13 15:10:40 +0530

    Completed task 3
    - completed the code for getting the book details based on the author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,8 +30,21 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const author = req.params.author;
+    if (author !== "" && author.length > 0) {
+        const bookEntries = Object.entries(books);
+        const booksByAuthor = bookEntries.filter(([_, values]) => {
+            return values.author.toLowerCase() === author.toLowerCase();
+        });
+        if (booksByAuthor.length > 0) {
+            const books = booksByAuthor.map(([_, values]) => values);
+            return res.send(books);
+        } else {
+            return res.status(400).send({ message: "Book not found" });
+        }
+    } else {
+        return res.status(400).send({ message: "Author should not be blank" });
+    }
 });
 
 // Get all books based on title
