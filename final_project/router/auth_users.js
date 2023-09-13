77546fc2f6c649e70342629c0621f168eb2a2974
R0N-C0D3R <roncoder261@gmail.com>
2023-09-13 23:25:16 +0530

    Completed task 9
    - completed the code for deleting a book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -74,6 +74,32 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     }
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const { isbn } = req.params;
+    if (!isbn || isNaN(isbn)) {
+        return res.status(400).send({
+            message: "Error deleting review. Valid ISBN not provided",
+        });
+    }
+    const bookByIsbn = books[isbn];
+    if (bookByIsbn) {
+        const username = req.session.authorization.username;
+        const reviewByUsername = bookByIsbn.reviews[username];
+        if (reviewByUsername) {
+            delete bookByIsbn.reviews[username];
+            return res.status(200).send({
+                message: "Review deleted",
+            });
+        } else {
+            return res.status(400).send({
+                message: "Error deleting review. Review not found",
+            });
+        }
+    } else {
+        return res.status(400).send({ message: "Error deleting review." });
+    }
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
